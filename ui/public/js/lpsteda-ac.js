67943c6de884cf5f808b6bbb999e8fa4672b9b2c
Abdullah Alshaibani <eggandroid@gmail.com>
2016-04-14 00:53:27 -0400

    did the autocomplete and it includes MIV

---
+++ ui/public/js/lpsteda-ac.js
@@ -0,0 +1,20 @@
+function initializeAC(){
+
+
+
+	CodeMirror.commands.autocomplete = function(cm) {
+
+	    CodeMirror.showHint(cm, CodeMirror.hint.javascript);
+	    
+	};
+	editor.on("keyup", function (cm, event) {
+        if (!cm.state.completionActive && /*Enables keyboard navigation in autocomplete list*/
+            event.keyCode != 13 &&        /*Enter - do not open autocomplete list just after item has been selected in it*/ 
+            event.keyCode != 8) {
+            CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
+        }
+    });
+
+
+}
+
--- ui/public/js/lpsteda.js
+++ ui/public/js/lpsteda.js
@@ -16,6 +16,8 @@ $(document).ready(function(){
 
 	// CM
 	initializeCM();
+	
+	initializeAC();
 
 	initLPSupport();
 	
