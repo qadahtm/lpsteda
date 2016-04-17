1bde4036852fa7e33bd970e9103493dd157a1792
Abdullah Alshaibani <eggandroid@gmail.com>
2016-04-17 19:19:21 -0400

    edited the auto complete to remove some base functions

--- ui/public/js/lpsteda-ac.js
+++ ui/public/js/lpsteda-ac.js
@@ -10,7 +10,9 @@ function initializeAC(){
 	editor.on("keyup", function (cm, event) {
         if (!cm.state.completionActive && /*Enables keyboard navigation in autocomplete list*/
             event.keyCode != 13 &&        /*Enter - do not open autocomplete list just after item has been selected in it*/ 
-            event.keyCode != 8) {
+            event.keyCode != 8 &&
+            event.keyCode != 27 &&
+            event.keyCode != 186) {
             CodeMirror.commands.autocomplete(cm, null, {completeSingle: false});
         }
     });
